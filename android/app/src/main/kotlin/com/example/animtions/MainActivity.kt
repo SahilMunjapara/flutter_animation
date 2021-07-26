4785c7d122339057bb741bfdfcf99e15a2a0df07
SahilMunjapara <sahilmunjapara33@gmail.com>
2021-07-26 14:16:03 +0530

    My First Commit

---
+++ android/app/src/main/kotlin/com/example/animtions/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.animtions
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
+
+void main() => runApp(MaterialApp(
+      home: MyApp(),
+    ));
+
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<MyApp> with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = new AnimationController(
+        duration: const Duration(milliseconds: 4000), vsync: this);
+    animation = new Tween(begin: 0.0, end: 400.0).animate(controller);
+    animation.addListener(() {
+      setState(() {
+        // the state of the animation has changed
+      });
+    });
+
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Name here'),
+      ),
+      body: Container(
+        height: animation.value,
+        width: animation.value,
+        child: Center(
+          child: FlutterLogo(
+            size: 300.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
